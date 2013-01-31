3f181b5aea6d461ac245074e081c53f044361dd7
Mohammad Hosein Saadatfar <MH.Saadatfar@gmail.com>
2013-01-31 10:04:37 +0330

    Update admin site/main action view
    - Go to index.php if laready loged in
    - Else go to login page

--- protected/admin/views/default/index.php
+++ protected/admin/views/default/index.php
@@ -1,21 +1,16 @@
 <?php
 /**
- * Admin site action view
+ * Admin site/main action view
  * @author Mohammad Hosein Saadatfar
  * @copyright Copyright &copy; Mohammad Hosein Saadatfar 2012-
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  *
  */
 ?>
-
-
-<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
-
-<p>
-This is the view content for action "<?php echo $this->action->id; ?>".
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
-</p>
-<p>
-You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>
+<?php 
+if (Yii::app()->user->id=='admin'){
+	$this->redirect('index.php');
+}else{
+	$this->redirect(array('login/login'));
+}
+?>
