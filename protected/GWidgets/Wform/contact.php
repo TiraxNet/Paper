819db6950d8da672cf17496b53dd4c0287f77b10
Saadatfar <mh.saadatfar@gmail.com>
2013-11-14 17:04:29 +0330

    Include subject in email subject.

--- protected/GWidgets/Wform/contact.php
+++ protected/GWidgets/Wform/contact.php
@@ -6,7 +6,7 @@ $message->setBody(array('name'=>$name,
 		'email'=>$email,
 		'subject'=>$subject,
 		'body'=>$body), 'text/html');
-$message->subject = 'New Contact From your Website';
+$message->subject = 'New Contact From Your Website: '.$subject;
 $message->addTo(Yii::app()->params['AdminMail']);
 $message->from = 'info@tiraxnet.ir';
 Yii::app()->mail->send($message);
